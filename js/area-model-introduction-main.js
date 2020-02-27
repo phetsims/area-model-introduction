524ccbcc4c8354604bc5e9fd33522671ddd74a7c
samreid <reids@colorado.edu>
2020-02-27 11:38:05 -0700

    Add repo to migration list, see https://github.com/phetsims/chipper/issues/875

--- js/area-model-introduction-main.js
+++ js/area-model-introduction-main.js
@@ -5,33 +5,29 @@
  *
  * @author Jonathan Olson <jonathan.olson@colorado.edu>
  */
-define( require => {
-  'use strict';
 
-  // modules
-  const MultiplyScreen = require( 'AREA_MODEL_COMMON/screens/MultiplyScreen' );
-  const PartitionScreen = require( 'AREA_MODEL_COMMON/screens/PartitionScreen' );
-  const Sim = require( 'JOIST/Sim' );
-  const SimLauncher = require( 'JOIST/SimLauncher' );
+import MultiplyScreen from '../../area-model-common/js/screens/MultiplyScreen.js';
+import PartitionScreen from '../../area-model-common/js/screens/PartitionScreen.js';
+import Sim from '../../joist/js/Sim.js';
+import SimLauncher from '../../joist/js/SimLauncher.js';
+import areaModelIntroductionStrings from './area-model-introduction-strings.js';
 
-  // strings
-  const areaModelIntroductionTitleString = require( 'string!AREA_MODEL_INTRODUCTION/area-model-introduction.title' );
+const areaModelIntroductionTitleString = areaModelIntroductionStrings[ 'area-model-introduction' ].title;
 
-  const simOptions = {
-    credits: {
-      leadDesign: 'Amy Hanson, Amanda McGarry',
-      softwareDevelopment: 'Jonathan Olson',
-      team: 'Karina Hensberry, Susan Miller, Ariel Paul, Kathy Perkins',
-      qualityAssurance: 'Steele Dalton, Bryce Griebenow, Ethan Johnson, Liam Mulhall, Ben Roberts, Jacob Romero, Ethan Ward, Clara Wilson, Kathryn Woessner',
-      graphicArts: 'Mariah Hermsmeyer, Amanda McGarry, Diana L\u00f3pez Tavares'
-    }
-  };
+const simOptions = {
+  credits: {
+    leadDesign: 'Amy Hanson, Amanda McGarry',
+    softwareDevelopment: 'Jonathan Olson',
+    team: 'Karina Hensberry, Susan Miller, Ariel Paul, Kathy Perkins',
+    qualityAssurance: 'Steele Dalton, Bryce Griebenow, Ethan Johnson, Liam Mulhall, Ben Roberts, Jacob Romero, Ethan Ward, Clara Wilson, Kathryn Woessner',
+    graphicArts: 'Mariah Hermsmeyer, Amanda McGarry, Diana L\u00f3pez Tavares'
+  }
+};
 
-  SimLauncher.launch( function() {
-    const sim = new Sim( areaModelIntroductionTitleString, [
-      new MultiplyScreen(),
-      new PartitionScreen()
-    ], simOptions );
-    sim.start();
-  } );
+SimLauncher.launch( function() {
+  const sim = new Sim( areaModelIntroductionTitleString, [
+    new MultiplyScreen(),
+    new PartitionScreen()
+  ], simOptions );
+  sim.start();
 } );
---
+++ js/area-model-introduction-strings.js
@@ -0,0 +1,14 @@
+// Copyright 2020, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+
+import getStringModule from '../../chipper/js/getStringModule.js';
+import areaModelIntroduction from './areaModelIntroduction.js';
+
+const areaModelIntroductionStrings = getStringModule( 'AREA_MODEL_INTRODUCTION' );
+
+areaModelIntroduction.register( 'areaModelIntroductionStrings', areaModelIntroductionStrings );
+
+export default areaModelIntroductionStrings;
