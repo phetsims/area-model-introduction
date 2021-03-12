d984072aa0fd670e3a5f60e5c80e222ecbee0850
samreid <reids@colorado.edu>
2021-03-12 10:30:50 -0700

    ESLint: Autofix prefer-arrow-callback rule, see https://github.com/phetsims/chipper/issues/814

--- js/area-model-introduction-main.js
+++ js/area-model-introduction-main.js
@@ -24,7 +24,7 @@ const simOptions = {
   }
 };
 
-simLauncher.launch( function() {
+simLauncher.launch( () => {
   const sim = new Sim( areaModelIntroductionTitleString, [
     new MultiplyScreen(),
     new PartitionScreen()
