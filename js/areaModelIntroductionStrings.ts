2755d6d6e9ccaf078a8773554da217b5ebd7bd09
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:34 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/areaModelIntroductionStrings.ts
+++ js/areaModelIntroductionStrings.ts
@@ -5,11 +5,13 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import areaModelIntroduction from './areaModelIntroduction.js';
 
 type StringsType = {
   'area-model-introduction': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   }
 };
 
