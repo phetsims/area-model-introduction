f76385a10830a662a203d110119ec453c39986ab
Luisav1 <luisa.vargassuarez@ucalgary.ca>
2024-04-09 14:31:35 -0600

    Update credits. See https://github.com/phetsims/area-model-introduction/issues/9.

--- js/area-model-introduction-main.js
+++ js/area-model-introduction-main.js
@@ -17,9 +17,9 @@ const areaModelIntroductionTitleStringProperty = AreaModelIntroductionStrings[ '
 const simOptions = {
   credits: {
     leadDesign: 'Amy Hanson, Amanda McGarry',
-    softwareDevelopment: 'Jonathan Olson',
+    softwareDevelopment: 'Jonathan Olson, Luisa Vargas',
     team: 'Karina Hensberry, Susan Miller, Ariel Paul, Kathy Perkins',
-    qualityAssurance: 'Steele Dalton, Bryce Griebenow, Ethan Johnson, Liam Mulhall, Ben Roberts, Jacob Romero, Ethan Ward, Clara Wilson, Kathryn Woessner',
+    qualityAssurance: 'Steele Dalton, Jaron Droder, Bryce Griebenow, Ethan Johnson, Liam Mulhall, Ben Roberts, Jacob Romero, Nancy Salpepi, Ethan Ward, Clara Wilson, Kathryn Woessner',
     graphicArts: 'Mariah Hermsmeyer, Amanda McGarry, Diana L\u00f3pez Tavares'
   }
 };
