4caeed82cbf839b413fda2b96e08e0d837113954
Jonathan Olson <jonathan.olson@colorado.edu>
2026-02-17 10:02:01 -0700

    Update Jonathan Olson author annotation, see https://github.com/phetsims/molecule-polarity/issues/305

--- js/area-model-introduction-main.js
+++ js/area-model-introduction-main.js
@@ -3,7 +3,7 @@
 /**
  * Main entry point for the sim.
  *
- * @author Jonathan Olson <jonathan.olson@colorado.edu>
+ * @author Jonathan Olson (PhET Interactive Simulations)
  */
 
 import MultiplyScreen from '../../area-model-common/js/screens/MultiplyScreen.js';
